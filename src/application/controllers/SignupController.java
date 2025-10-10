624dbdd643c1bc1624dc82cd04cf9573a59aaedd
Asus <ryanehlm0@gmail.com>
2025-10-10 12:18:40 -0400

    cleaned up directory, moved models and services folders into the src folder.

--- src/application/controllers/SignupController.java
+++ src/application/controllers/SignupController.java
@@ -4,30 +4,30 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class LoginController {
-	
-	String name;
-	
-	@FXML
-	private TextField username;
-	private PasswordField password;
+public class SignupController {
 
-	private Scene previousScene;
-	
-	public void login(ActionEvent event) {
-		name = username.getText();
-	}
-	
-	public void setPreviousScene(Scene scene) {
+    @FXML
+    public TextField nameField;
+    @FXML
+    public TextField emailField;
+    @FXML
+    public PasswordField passwordField;
+    @FXML
+    public PasswordField confirmPasswordField;
+    @FXML
+    public DatePicker birthdatePicker;
+    @FXML
+    private Scene previousScene;
+
+    public void setPreviousScene(Scene scene) {
         this.previousScene = scene;
     }
-	
-    @FXML
-    private void goBack(ActionEvent event) {
+    public void goBack(ActionEvent event) {
         if (previousScene != null) {
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(previousScene);
