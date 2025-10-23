b000ede10c2b0adaf1499d5dd7257acbc71a57ef
Jerome Kithinji <kithinjijerome@gmail.com>
2025-10-23 19:03:07 -0400

    Added comments to tests

--- src/test/PassTest.java
+++ src/test/PassTest.java
@@ -11,18 +11,23 @@ import java.time.LocalTime;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PassTest {
+    // Tests that a NIGHT pass expired at 06:00 the next calendar day relative to its start time.
     @Test
     void nightPassExpiresSixNextDay() {
         Pass p = new Pass(ZoneType.A, PassDuration.NIGHT, false);
         var start = p.getStartDateTime();
         assertEquals(start.toLocalDate().plusDays(1).atTime(LocalTime.of(6,0)), p.getExpiryDateTime());
     }
+
+    // Tests that a DAY pass expires at 23:59:59 on the same calendar day it starts.
     @Test
     void dayPassExpiresEndOfDay() {
         Pass p = new Pass(ZoneType.A, PassDuration.DAY, false);
         var startDay = p.getStartDateTime().toLocalDate();
         assertEquals(startDay.atTime(23,59,59), p.getExpiryDateTime());
     }
+
+    // Tests if a pass becomes invalid once its expiryDateTime is in the past.
     @Test
     void passBecomesInvalidAfterExpiry() throws Exception {
         Pass p = new Pass(ZoneType.A, PassDuration.DAY, false);
